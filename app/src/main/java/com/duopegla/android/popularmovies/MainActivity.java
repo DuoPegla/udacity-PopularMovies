862f868e28cd33a9478484012fa514c1eae20ba8
DuoPegla <josip.sarlija@gmail.com>
2017-02-04 19:06:45 +0100

    Tests connection and data retrieval from the moviedb

--- app/src/main/java/com/duopegla/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/duopegla/android/popularmovies/MainActivity.java
@@ -1,15 +1,26 @@
 package com.duopegla.android.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("KEY", getResources().getString(R.string.themoviedb_api_key));
 
-        mImageView = (ImageView) findViewById(R.id.imageView);
+        mImageView = (ImageView) findViewById(R.id.iv_image_test);
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(mImageView);
+
+        mTextView = (TextView) findViewById(R.id.tv_movie_test);
+        String apiKey = getResources().getString(R.string.themoviedb_api_key);
+        String request = "https://api.themoviedb.org/3/movie/550?api_key=" + apiKey;
+        new FetchMovieDataTask().execute(request);
+
+    }
+
+    public class FetchMovieDataTask extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            if (params.length == 0)
+            {
+                return null;
+            }
+
+            Uri builtUri = Uri.parse(params[0]).buildUpon().build();
+            URL url = null;
+            try
+            {
+                url = new URL(builtUri.toString());
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+
+            Log.d("URL", url.toString());
+
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    InputStream in = urlConnection.getInputStream();
+
+                    Scanner scanner = new Scanner(in);
+                    scanner.useDelimiter("\\A");
+
+                    boolean hasInput = scanner.hasNext();
+                    if (hasInput) {
+                        return scanner.next();
+                    } else {
+                        return null;
+                    }
+                } catch (IOException e) {
+                    return null;
+                } finally {
+                    urlConnection.disconnect();
+                }
+            }
+            catch (IOException e)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null)
+            {
+                mTextView.setText(s);
+            }
+            else
+            {
+                Log.d("ERROR", "Returned null result");
+            }
+        }
     }
 }
