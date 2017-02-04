a710ec0b04c39a799f6bad9d7498657a1ca06c40
DuoPegla <josip.sarlija@gmail.com>
2017-02-04 18:32:10 +0100

    Added picasso image library

--- app/src/main/java/com/duopegla/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/duopegla/android/popularmovies/MainActivity.java
@@ -3,13 +3,21 @@ package com.duopegla.android.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("KEY", getResources().getString(R.string.themoviedb_api_key));
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(mImageView);
     }
 }
